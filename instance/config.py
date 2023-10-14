d24b6dc00fad0af9086b98e0a3abf818f2d9a88a
Fabián Payan <f.payan@uniandes.edu.co>
2023-10-14 16:05:34 -0500

    Se agrega configuración de instancia

---
+++ instance/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+OUR_HOST=os.getenv("DB_HOST", "db")
+OUR_DB=os.getenv("DB_DB", "conversion_db")
+OUR_USER=os.getenv("DB_USER", "admin_db")
+OUR_PORT=os.getenv("DB_PORT", "5432")
+OUR_PW=os.getenv("DB_PW", "admin_conversion_tool_db")
+OUR_SECRET=os.getenv("SECRET", "conversiontooldb")
+OUR_JWTSECRET=os.getenv("JWTSECRET", "conversiontooldb")
+
+DEBUG = False
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(OUR_USER, OUR_PW, OUR_HOST, OUR_PORT, OUR_DB)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+JWT_SECRET_KEY = OUR_JWTSECRET
+SECRET_KEY = OUR_SECRET
+PROPAGATE_EXCEPTIONS = True
