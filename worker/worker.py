1dde5430266508766865418d7618a83a5617255a
Fabián Payan <f.payan@uniandes.edu.co>
2023-11-09 19:02:00 -0500

    se agrega prueba google storage

--- worker/worker.py
+++ worker/worker.py
@@ -5,6 +5,7 @@ from moviepy.editor import VideoFileClip
 import os
 import psycopg2
 from config import SQLALCHEMY_DATABASE_URI
+from google.cloud import storage
 
 
 celery_app = Celery("process_task_converter", broker='redis://redis:6379/0')
@@ -29,11 +30,39 @@ def convertir_video(fileName, newFormat, task_id, user_id):
         
         video = VideoFileClip(fileName)
         video.write_videofile(output_video_path, codec=codec_dic[newFormat])
+        
+        #####################
+        CLOUD_STORAGE_BUCKET = "gs://cloud-conversion-tool-bucket-g8"
+        
+        uploaded_file = VideoFileClip(fileName)
+        
+        # Create a Cloud Storage client.
+        gcs = storage.Client()
+
+        # Get the bucket that the file will be uploaded to.
+        bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
+
+        # Create a new blob and upload the file's content.
+        blob = bucket.blob(uploaded_file.filename)
+
+        blob.upload_from_string(
+            uploaded_file.read(),
+            content_type=uploaded_file.content_type
+        )
+
+        # Make the blob public. This is not necessary if the
+        # entire bucket is public.
+        # See https://cloud.google.com/storage/docs/access-control/making-data-public.
+        blob.make_public()
+
+        # The public URL can be used to directly access the uploaded file via HTTP.
+        logging.info(blob.public_url) 
+
         new_status = "PROCESSED"
         update = update_task_status(task_id,new_status,output_video_path)
         logging.info(update)
         
-        return f'Conversión exitosa. El video se ha guardado en {output_video_path}'
+        return f'Conversión exitosa. El video se ha guardado en {output_video_path} - {blob.public_url}'
     except Exception as e:
         return f'Error al convertir el video: {str(e)}'
 
