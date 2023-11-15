9154929b56eadc793cf34f2fe81b30717a92fd27
esgonzalez_meli <esneider.gonzalez@mercadolibre.com.co>
2023-11-15 00:39:30 -0500

    update send task to process func

--- conversion_app/vistas/vistas.py
+++ conversion_app/vistas/vistas.py
@@ -161,18 +161,17 @@ class VistaTask(Resource):
 
 #@celery.task(name="convert_process")
 def send_task_to_process(*args):
-    logging.info(str(args))
+    args_str = ",".join(map(str, args))
+    logging.info(args_str)
     publisher = pubsub_v1.PublisherClient()
     # The `topic_path` method creates a fully qualified identifier
     # in the form `projects/{project_id}/topics/{topic_id}`
     topic_path = publisher.topic_path(GCP_PROJECT_ID, GCP_TOPIC_ID)
-    for n in range(1, 10):
-        data_str = f"Message number {n}"
-        # Data must be a bytestring
-        data = data_str.encode("utf-8")
-        # When you publish a message, the client returns a future.
-        future = publisher.publish(topic_path, data)
-        print(future.result())
+    # Data must be a bytestring
+    data = args_str.encode("utf-8")
+    # When you publish a message, the client returns a future.
+    future = publisher.publish(topic_path, data)
+    print(future.result())
 
     print(f"Published messages to {topic_path}.")
 
